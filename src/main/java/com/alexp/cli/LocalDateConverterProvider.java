726db05df44bc21b72f0c313fd3adbab46e4e441
Aleksandar Pavlovic <alex.pavlovic92@gmail.com>
2022-01-15 11:14:49 +0100

    parsing cli arguments

---
+++ src/main/java/com/alexp/cli/LocalDateConverterProvider.java
@@ -0,0 +1,32 @@
+package com.alexp.cli;
+
+import com.github.rvesse.airline.model.ArgumentsMetadata;
+import com.github.rvesse.airline.model.OptionMetadata;
+import com.github.rvesse.airline.parser.ParseState;
+import com.github.rvesse.airline.types.TypeConverter;
+import com.github.rvesse.airline.types.TypeConverterProvider;
+import com.github.rvesse.airline.types.numerics.NumericTypeConverter;
+
+import java.time.LocalDate;
+
+public class LocalDateConverterProvider implements TypeConverterProvider {
+    @Override
+    public <T> TypeConverter getTypeConverter(OptionMetadata option, ParseState<T> state) {
+        return LOCAL_DATE_CONVERTER;
+    }
+
+    @Override
+    public <T> TypeConverter getTypeConverter(ArgumentsMetadata arguments, ParseState<T> state) {
+        return LOCAL_DATE_CONVERTER;
+    }
+
+    private static final TypeConverter LOCAL_DATE_CONVERTER = new TypeConverter() {
+        @Override
+        public Object convert(String name, Class<?> type, String value) {
+            return LocalDate.parse(value);
+        }
+
+        @Override
+        public void setNumericConverter(NumericTypeConverter converter) {}
+    };
+}
